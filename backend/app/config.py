a914978ac1e2faa96898c03f74789619d66e26d1
SadPeeep <150479984+SadPeeep@users.noreply.github.com>
2026-08-10 23:30:47 +0500

    feat: add SPOTIFY_REDIRECT_URI

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     BOT_TOKEN: str
     SPOTIFY_CLIENT_ID: str
     SPOTIFY_CLIENT_SECRET: str
+    SPOTIFY_REDIRECT_URI: str = "https://spotiffy-tma-production.up.railway.app/api/spotify/callback"
     DATABASE_URL: str = "sqlite+aiosqlite:///./spotify_tma.db"
     CORS_ORIGINS: List[str] = ["*"]
 
